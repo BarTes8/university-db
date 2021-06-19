7d785de7974dda46a3261af7b4999a82f40c6efe
BarTes8 <barbara.tessema@gmail.com>
2021-06-19 14:08:58 +0200

    added XXXX some changes

---
+++ build/CMakeFiles/universityDB.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bartes/university/AllStudents.cpp" "/home/bartes/university/build/CMakeFiles/universityDB.dir/AllStudents.cpp.o"
+  "/home/bartes/university/Student.cpp" "/home/bartes/university/build/CMakeFiles/universityDB.dir/Student.cpp.o"
+  "/home/bartes/university/main.cpp" "/home/bartes/university/build/CMakeFiles/universityDB.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/universityDB.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/universityDB.dir/AllStudents.cpp.o"
+  "CMakeFiles/universityDB.dir/Student.cpp.o"
+  "CMakeFiles/universityDB.dir/main.cpp.o"
+  "universityDB"
+  "universityDB.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/universityDB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
