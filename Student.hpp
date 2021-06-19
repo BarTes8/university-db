578c6bae7fe6828a3fa455d192d64ed2ad53a333
BarTes8 <barbara.tessema@gmail.com>
2021-06-19 10:22:01 +0200

    created class Student

---
+++ Student.hpp
@@ -0,0 +1,11 @@
+#include <string>
+
+class Student {
+private:
+    std::string name_ {};
+    std::string surname_ {};
+    std::string address_ {};
+    std::string indexNumber_ {};
+    std::string personalIdentityNumber_ {};
+    char gender_ {};
+};
