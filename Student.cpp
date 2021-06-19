2b79947243598dae1ffa693a8854df334292e343
Artur Michałowski <artur.michalowski93@gmail.com>
2021-06-19 10:41:46 +0200

    Added addStudent()

---
+++ Student.cpp
@@ -0,0 +1,18 @@
+#include "Student.hpp"
+
+void Student::addStudent() {
+    std::cout << "Name: " ;
+    getline(std::cin, name_);
+    std::cout << "Surname: " ;
+    getline(std::cin, surname_);
+    std::cout << "Address: " ;
+    getline(std::cin, address_);
+    std::cout << "Index number: " ;
+    getline(std::cin, indexNumber_);
+    std::cout << "Personal identity number: " ;
+    getline(std::cin, personalIdentityNumber_);
+    std::cout << "Gender: " ;
+    std::cin >> gender_;
+    std::cin.clear();
+    std::cin.ignore();
+};
--- Student.hpp
+++ Student.hpp
@@ -1,6 +1,11 @@
+#pragma once
+
 #include <string>
+#include <iostream>
 
 class Student {
+public:
+    void addStudent();
 private:
     std::string name_ {};
     std::string surname_ {};
