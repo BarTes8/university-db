a0e4e6e8b4a45688a5b5a57bfbfa87b0950063b2
Artur Michałowski <artur.michalowski93@gmail.com>
2021-06-19 11:24:10 +0200

    Added main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "Student.hpp"
 
 int main() {
-    std::cout << "Hello world\n";
+    Student student1("Adam", "Nowak", "Wroclaw", "12445", "12312421", 'm');
+    std::cout << student1.getName() << '\n';
+    std::cout << student1.getSurname() << '\n';
+    std::cout << student1.getAddress() << '\n';
+    std::cout << student1.getIndexNumber() << '\n';
+    std::cout << student1.getPersonalIdentityNumber() << '\n';
+    std::cout << student1.getGender() << '\n';
 }
