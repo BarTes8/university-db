21ef0431243dd4c960052af10f997a3099fb1d6f
Artur Michałowski <artur.michalowski93@gmail.com>
2021-06-19 11:19:43 +0200

    Added constructor for Student class, added some tests in main

---
+++ AllStudents.cpp
@@ -0,0 +1,21 @@
+#include "AllStudents.hpp"
+
+void AllStudents::addStudent() {
+    
+    
+};
+
+// std::cout << "Name: " ;
+    // getline(std::cin, name_);
+    // std::cout << "Surname: " ;
+    // getline(std::cin, surname_);
+    // std::cout << "Address: " ;
+    // getline(std::cin, address_);
+    // std::cout << "Index number: " ;
+    // getline(std::cin, indexNumber_);
+    // std::cout << "Personal identity number: " ;
+    // getline(std::cin, personalIdentityNumber_);
+    // std::cout << "Gender: " ;
+    // std::cin >> gender_;
+    // std::cin.clear();
+    // std::cin.ignore();
---
+++ AllStudents.hpp
@@ -0,0 +1,13 @@
+#include "Student.hpp"
+#include <vector>
+
+class AllStudents {
+public:
+    AllStudents() = default;
+ 
+    void addStudent();
+
+private:
+    std::vector<Student> students_ {};
+
+};
